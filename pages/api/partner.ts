c94020a43015f3400cf80a1d37ed9a26c27f604d
Victor Brambati <victorbrambati@gmail.com>
2021-04-14 14:08:53 -0300

    Feature create /partner api route

--- pages/api/partner.ts
+++ pages/api/partner.ts
@@ -1,5 +1,67 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import connect from '../../utils/database';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
+interface ErrorResponseType {
+  error: string;
 }
+
+interface SuccessResponseType {
+  firstName: string;
+  lastName: string;
+  participation: number;
+  _id: string;
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ErrorResponseType | SuccessResponseType>
+): Promise<void> => {
+  if (req.method === 'POST') {
+    const { firstName, lastName, participation } = req.body;
+
+    if (!firstName || !lastName || !participation) {
+      res.status(400).json({ error: 'Missing body paramter' });
+      return;
+    }
+
+    if (participation > 100) {
+      res
+          .status(400)
+          .json({ error: 'participation is greater than 100' });
+        return;
+    }
+
+    const { db } = await connect();
+
+    const participationTotal = db.collection('partner').aggregate([
+      {
+        $group: {
+          _id: '12345',
+          total: {
+            $sum: '$participation',
+          },
+        },
+      },
+    ]);
+
+    const ar = await participationTotal.toArray();
+    const ar2 = ar[0];
+
+    if (ar2 !== undefined) {
+      if (ar2.total + participation > 100) {
+        res
+          .status(400)
+          .json({ error: 'participation total is greater than 100' });
+        return;
+      }
+    }
+    const response = await db.collection('partner').insertOne({
+      firstName,
+      lastName,
+      participation,
+    });
+    res.status(200).json(response.ops[0]);
+  } else {
+    res.status(400).json({ error: 'Wrong request' });
+  }
+};
--- utils/database.ts
+++ utils/database.ts
@@ -1,3 +1,13 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.DATABASE_URL);
+const client = new MongoClient(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export default async function connect() {
+  if (!client.isConnected()) await client.connect();
+
+  const db = client.db('ChallengeFullStack');
+  return { db, client };
+}
